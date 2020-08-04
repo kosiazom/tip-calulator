b2ca113f71d476e114f61dbe64311f1e66f20590
Kosi Akporji <kosiazom@gmail.com>
2020-08-04 13:52:04 -0400

    changed to appropriate datatypes

--- index.js
+++ index.js
@@ -7,11 +7,15 @@ document.addEventListener("DOMContentLoaded", (e) => {
 
 function update() {//grabbing the values here to be updated and calling it in the 
     //listener
-    let bill = document.getElementById('yourBill').value
-    let tipPercent = document.getElementById('tipInput').value
-    let split = document.getElementById('splitInput').value
+    let bill = Number(document.getElementById('yourBill').value)
+    let tipPercent = Number(document.getElementById('tipInput').value)
+    let split = Number(document.getElementById('splitInput').value)
+    
     // console.log({bill, tipInput, split}) //when you console log curly brackets its sets it to an object
-    let tipValue = bill * (tipInput/100)
+    let tipValue = bill * (tipPercent / 100)
+    let tipEach = tipValue /split;
+    let newBillEach = (bill + tipValue) / split
+    console.log(newBillEach)  
 }
 
     let container = document.getElementById('container')
