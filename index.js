68a4c5498023bb64048dfcd4848298de588972c4
Kosi Akporji <kosiazom@gmail.com>
2020-08-04 20:49:28 -0400

    done

--- index.js
+++ index.js
@@ -3,19 +3,46 @@ document.addEventListener("DOMContentLoaded", (e) => {
 
 });
 
+function formatMoney(value){
+    console.log(value)
 
+    value = Math.ceil(value * 100) / 100
+    console.log(value)
+    
+    value = value.toFixed(2)
+    return "$" + value
+}
+
+function formatSplit(value) {
+    if(value === 1) {
+        return value +  " person"
+    }
+    else {
+        return value + " people"
+    }
+}
 
 function update() {//grabbing the values here to be updated and calling it in the 
     //listener
     let bill = Number(document.getElementById('yourBill').value)
-    let tipPercent = Number(document.getElementById('tipInput').value)
+    let tipInput = Number(document.getElementById('tipInput').value)
     let split = Number(document.getElementById('splitInput').value)
     
     // console.log({bill, tipInput, split}) //when you console log curly brackets its sets it to an object
-    let tipValue = bill * (tipPercent / 100)
+    let tipValue = bill * (tipInput / 100)
     let tipEach = tipValue /split;
     let newBillEach = (bill + tipValue) / split
-    console.log(newBillEach)  
+    let tipPercent = document.getElementById("tipPercent")
+    tipPercent.innerHTML = tipInput + "%"
+
+
+    document.getElementById('tipValue').innerHTML = formatMoney(tipValue)
+    document.getElementById('totalWithTip').innerHTML = formatMoney(bill + tipValue)
+    document.getElementById('splitValue').innerHTML = formatSplit(split)
+    document.getElementById('billPerson').innerHTML = formatMoney(newBillEach)
+    document.getElementById('tipPerson').innerHTML = formatMoney(tipEach)
+ 
+
 }
 
     let container = document.getElementById('container')
